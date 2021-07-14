43fcc0eac636eb69037885d8763e1a87eae3c197
ken <ken.mbira@student.moringaschool.com>
2021-07-14 08:47:38 +0300

    enter vaiables and functions to be used

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var gram;
+var litre;
+var tablespoon;
+//var pound= prompt("Enter value in pounds:")
+//var gallon = prompt("Enter value in gallons:")
+//var cup = prompt("Enter value in cups")
+//var pint = prompt("Enter value in pints")
+var ounce = prompt("Enter value in ounces")
+
+//Pounds to Grams
+//var poundGram = () => {alert(pound*453+" grams");};
+//poundGram();
+
+//Gallons to Litres
+//var gallonLitre = () => {alert(gallon*3.8 + " litres");};
+//gallonLitre();
+
+//Cups to Litres
+//var cupLitre = () => {alert(cup*0.2 + " litres");};
+//cupLitre();
+
+//Pint to Tablespoon
+//var pintTablespoon = () => {alert(pint*32 + " tablespoon");};
+//pintTablespoon();
+
+//Ounce to Gram
+var ounceGram = () => {alert(ounce*28 + " grams");};
+ounceGram();
